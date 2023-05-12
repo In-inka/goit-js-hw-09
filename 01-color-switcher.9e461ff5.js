7cf5497544013c7721a145b19ff147bef83c57a5
In-inka <119360847+In-inka@users.noreply.github.com>
2023-05-12 19:51:51 +0000

    Deploying to gh-pages from @ In-inka/goit-js-hw-09@85444aabc066979cfbbc8d82f0e7ceb9943b3fc4 🚀

--- 01-color-switcher.9e461ff5.js
+++
@@ -1,2 +0,0 @@
-!function(){var t,e=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.disabled=!1,d.disabled=!0,e.addEventListener("click",(function(){t=setInterval(a,1e3),e.disabled=!0,e.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){clearInterval(t),t=null,e.disabled=!1,d.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.9e461ff5.js.map
